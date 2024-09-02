8c0f33bcc58b4f483b38ce9cd29dc582fda4af90
Will Ramires <97480446+willramires@users.noreply.github.com>
2024-09-02 11:19:31 -0300

    Update clients.js

--- Garantia/database/clients.js
+++ Garantia/database/clients.js
@@ -1,12 +1,29 @@
 const clients = [
-    {
-        id: '1',
-        name: 'will',
-    },
-    {
-        id: '2',
-        name: "petrus",
-    }
+   {
+    id: 1,
+    cliente: {
+      nome: "Will",
+      cpf: "000.000.000-00",
+      contato: {
+        numero: "00 00000-0000",
+        email: "willgabrielramires@gmail.com",
+      }
+      endereço: "rua aaa",
+      
+      informação da compra: {
+        data da compra: "01-01-2024",
+        tipo de produto: "Notebook",
+        tempo de grantia: "01-01-2024",
+        nota fiscal: "dasdas",
+        valor da compra: "2500,00",
+      }
+      informaçãodopedido: {
+        reparo: false,
+        troca: false,
+        estendergarantia: true,
+        devolução: false,
+      }
+    } 
 ]
 
 module.exports = {
