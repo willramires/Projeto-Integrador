72ba11b25805896a7a4c41ba4d5c36d115bda85c
Will Ramires <97480446+willramires@users.noreply.github.com>
2024-09-02 18:23:37 -0300

    Create clientes.js

---
+++ Garantia/database/clientes.js
@@ -0,0 +1,11 @@
+const clients = [
+   {
+    id: 1,
+      nome: "Will",
+      cpf: "000.000.000-00",
+      datanascimento: "01-01-2024"
+      numero: "00 00000-0000",
+      email: "willgabrielramires@gmail.com",
+      endereco: "rua aaa",
+   }
+]
