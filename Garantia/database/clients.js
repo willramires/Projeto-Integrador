83971571ca25b322c0c251fa3e4e7772992ae465
Will Ramires <97480446+willramires@users.noreply.github.com>
2024-09-04 09:47:03 -0300

    Update clients.js

--- Garantia/database/clients.js
+++ Garantia/database/clients.js
@@ -3,7 +3,7 @@ const clients = [
     id: 1,
       nome: "Will",
       cpf: "000.000.000-00",
-      datanascimento: "01-01-2024"
+      datanascimento: "01-01-2024",
       numero: "00 00000-0000",
       email: "willgabrielramires@gmail.com",
       endereco: "rua aaa",
