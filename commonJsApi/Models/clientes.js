c46ee45bfe57f2e0beabbd23d9a4c9b41df816f0
Will Ramires <97480446+willramires@users.noreply.github.com>
2024-09-09 11:30:30 -0300

    Add files via upload

---
+++ commonJsApi/Models/clientes.js
@@ -0,0 +1,18 @@
+const clientes = [
+    {
+        id: 1,
+        name: 'will',
+    },
+    {
+        id: 2,
+        name: "petrus",
+    },
+    {
+        id: 3,
+        name: "joao",
+    }
+]
+
+module.exports = {
+    clientes
+}
