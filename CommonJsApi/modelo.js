9e539c0bdea208e74d67dc12bd71fda0a351d469
Will Ramires <97480446+willramires@users.noreply.github.com>
2024-09-23 11:18:11 -0300

    Add files via upload

---
+++ CommonJsApi/modelo.js
@@ -0,0 +1,26 @@
+const clients = [
+   {
+    id: 1,
+      nome: "Will",
+      cpf: "000.000.000-00",
+      datanascimento: "01-01-2024",
+      numero: "00 00000-0000",
+      email: "willgabrielramires@gmail.com",
+      endereco: "rua aaa",
+      
+      datadacompra: "01-01-2024",
+      tipodeproduto: "Notebook",
+      tempodegrantia: "01-01-2024",
+      notafiscal: "dasdas",
+      valordacompra: "2500,00",
+      
+      reparo: false,
+      troca: false,
+      estendergarantia: true,
+      devolucao: false,
+      }
+]
+
+module.exports = {
+    clients
+}
