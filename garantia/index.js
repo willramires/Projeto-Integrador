5a5abb6f078ffb02b2c56649f6eeb2679e04238d
Will Ramires <97480446+willramires@users.noreply.github.com>
2024-10-30 10:56:28 -0300

    Update index.js

--- garantia/index.js
+++ garantia/index.js
@@ -30,6 +30,45 @@ app.get("/equipamentos", (req, res) => {
     });
 })
 
+app.post("/clientes", async (req,res) => {
+    const { nome, email, telefone, endereço } = req.body;
+    const result = await pool.query (
+      "INSERT INTO clientes (nome, email, telefone, endereço) VALUES ($1, $2, $3) RETURNING *" ,
+      [nome, email, telefone, endereço]
+    );
+    res.status(201).json(result.rows[0]);
+});
+
+app.delete("/equipamentos/:id", async (req,res) => {
+    const { id } = req.params;
+    await pool.query("DELETE FROM equipamentos WHERE id = $1", [id]);
+    res.status(204).send();
+});
+
+app.put("/clientes/:id", async (req, res) => {
+    const { id } = req.params;
+    const { nome, email, telefone, endereço } = req.body;
+    const result = await pool.query(
+         "UPDATE clientes SET nome = $1, email = $2, telefone = $3, endereço = $4 RETURNING *",
+        [nome, email, telefone, endereço]
+    );
+    res.json( result.rows [0]);
+
+});
+
+app.put("/equipamentos/:id", async (req, res) => {
+    const { id } = req.params;
+    const { nome, descrição, data_aquisição, modelo, serial_number, cliente_id } = req.body;
+    const result = await pool.query(
+         "UPDATE equipamentos SET nome = $1, descrição = $2, data_aquisição = $3, modelo = $4, serial_number = $5, cliente_id = $6 RETURNING *",
+        [nome, descrição, data_aquisição, modelo, serial_number, cliente_id]
+    );
+    res.json( result.rows [0]);
+
+});
+
+
+
 app.listen(port, () => {
     pool.connect().then((client) => {
         console.log('Connected to the database');
